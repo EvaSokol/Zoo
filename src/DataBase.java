9f3781340f10b076e8a7abf32812d615acc3f797
MASK <mask@pisem.net>
2015-09-29 23:40:38 +0300

    Getters created. Todo: Convert file data to animals.

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,9 +1,9 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 
-/**
- * Created by Eva Sokolyanskaya on 28/09/2015.
- */
+
 public class DataBase {
 
     static String filepath = "resources/my_animals.txt";
@@ -32,5 +32,25 @@ public class DataBase {
             e.printStackTrace();
         }
         return stringBuffer;
+    }
+
+    protected static LinkedList<String[]> getAnimalList() {
+        //get data from file to string
+        ArrayList<String> stringBuffer = ReadFile();
+        stringBuffer.remove(0); //remove first service string
+
+        LinkedList<String[]> animal_list = new LinkedList<String[]>();
+        Iterator<String> iterator = stringBuffer.iterator();
+        String[] temp_string_array;
+        String temp_string;
+        while (iterator.hasNext()) {
+            temp_string = iterator.next();
+            temp_string_array = temp_string.split(",");
+            animal_list.add(temp_string_array);
+            for (String str : temp_string_array) {
+//                System.out.println(str);
+            }
+        }
+        return animal_list;
     }
 }
